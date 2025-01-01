210e45e8385e5646c93ba707e6b6086df22f3438
vasusharrma <sharmavashu099@gmail.com>
2025-01-01 15:08:56 +0530

    esc to kj inoremap

---
+++ vim/kj-esc.sh
@@ -0,0 +1,22 @@
+    "vim.insertModeKeyBindings": [
+    {
+        "before": ["k", "j"],
+        "after": ["<Esc>"]
+    }
+],
+"[jsonc]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+},
+"vim.commandLineModeKeyBindingsNonRecursive": [
+
+],
+
+# These two are for highlighting for the same text we are looking for
+
+"vim.hlsearch": true,
+"vim.incsearch": true,
+"[javascript]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+}
+
+#  write it into the file > preferences > setting > setting.json 
